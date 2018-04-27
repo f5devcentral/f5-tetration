fdff54a4c0dfb52fd89eb5b424eba11fda64d48e
Sanjay <scshitole@gmail.com>
2018-04-27 16:02:06 -0700

    rules, scripts and json

---
+++ scripts/clean.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+echo " Please Enter contrl C to Quit \n"
+echo " This script will automatically delete and clean up iRule & IPFIX configuration \n"
+while :
+do
+  echo " \033[0;31mThis Script is Used to  *** Remove  configuration from BIG-IP  *** \033[0m \n"
+  echo " \033[1mPlease enter BIG-IP Management IP to Clean IPFIX configuration : \033[0m \c "
+  read BIGIP_MGMT_IP
+
+curl -s -sku admin:admin -H "Content-Type: application/json" -X DELETE https://$BIGIP_MGMT_IP/mgmt/tm/sys/log-config/publisher/ipfix-pub-1  > /dev/null
+echo " Removed ipfix-pub-1 log-config"
+curl -s -sku admin:admin -H "Content-Type: application/json" -X DELETE https://$BIGIP_MGMT_IP/mgmt/tm/sys/log-config/destination/ipfix/IPFIXLog > /dev/null
+echo " Removing IPFIXLog  "
+curl -s -sku admin:admin -H "Content-Type: application/json" -X DELETE  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/IPFIXPool > /dev/null
+echo " Removing IPFIXPool "
+
+ 
+
+
+#Dettach irule from Virtual Servers
+curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual  | python -m json.tool   >> top.txt
+        sleep 0.5
+
+        #Extract and Print all the virtual servers
+        awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="name"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' top.txt  >> good.txt # look for virtual server name in the only_name file
+        awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="ipProtocol"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' top.txt  >> goodp.txt # look for virtual server name in the only_name file
+
+        sed 's/"//g' good.txt >> better.txt
+        sed 's/"//g' goodp.txt >> betterp.txt
+        # Load file into array.
+        let i=0
+        # Read file better.txt line by line and put the details in array 
+        while IFS=$'\n' read -r line_data; do
+          viparray[i]="${line_data}"
+          ((++i))
+        done < better.txt
+      let j=0
+      while IFS=$'\n' read -r line_data; do
+            protoarray[j]="${line_data}"
+            ((++j))
+      done < betterp.txt
+      
+                    j=0
+                 while (( ${#viparray[@]} > j )); do
+
+                 	rm file.txt
+					curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} | python -m json.tool >> file.txt
+					value=$( grep -ic "Tetration" file.txt )
+					if [ $value -eq 2 ]
+					then
+ 
+					echo " Removing irule from Virtual Server "
+					curl -s -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} -d '{"rules":[""]}' > /dev/null
+					fi
+                    let j++
+                done
+rm *.txt > /dev/null
+#Remove irules from the BIG-IP
+echo "Remove irules from the BIG-IP"
+curl -s -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X DELETE  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule/Tetration_TCP_L4_ipfix > /dev/null
+curl -s -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X DELETE  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule/Tetration_UDP_L4_ipfix > /dev/null
+
+
+done 
+
+ 
---
+++ scripts/install.sh
@@ -0,0 +1,291 @@
+#!/bin/sh
+declare -a viparray
+declare -a poolarray
+declare -a membersarray
+rm *.txt
+echo "\033[0;31m Attention --->  Please Enter Contrl C to Quit this Program ....\033[0m \n"
+echo "\033[0;32m This script will automatically deploy the iRules required for Tetration \033[0m \n"
+while :
+do
+echo "\033[1mPlease enter BIG-IP Management IP : \033[0m \c "
+read   BIGIP_MGMT_IP
+echo "Please enter BIG-IP admin user   "
+#read BIGIP_ADMIN
+echo "Please enter BIG-IP admin password "
+#read BIGIP_PASS
+
+#Check if Irules Exists locally on your computer
+
+if [  \( -e Tetration_TCP_L4_ipfix.json \) -o \( -e Tetration_UDP_L4_ipfix.json \) ]
+then 
+	echo "\033[1mIrule Exists locally on your machine \033[0m .. hit enter...to proceed \c"
+	read 
+	sleep 2
+else
+	echo "\033[1mIrule missing ... locally will fetch from Github ...\033[0m \c"
+	curl https://raw.githubusercontent.com/f5devcentral/f5-tetration/master/irules/Tetration_TCP_L4_ipfix.json -o Tetration_TCP_L4_ipfix
+	curl https://raw.githubusercontent.com/f5devcentral/f5-tetration/master/irules/Tetration_UDP_L4_ipfix.json -o Tetration_UDP_L4_ipfix
+	
+     if [  \( -e Tetration_TCP_L4_ipfix.json \) -o \( -e Tetration_UDP_L4_ipfix.json \) ]
+     	then
+     	echo "\033[1mNow Irule Exists locally on your machine \033[0m .. hit enter...to proceed \c"
+     else
+	echo "Please visit https://github.com/f5devcentral/f5-tetration for additional information "
+	sleep 120
+	exit
+    fi
+
+fi
+
+
+## Check if iRule exists in BIG-IP
+echo "\033[1mChecking if irule Exists on BIG-IP ...... \033[0m \c "
+sleep 2
+
+response=$(curl --write-out %{http_code} --silent --output /dev/null -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule/Tetration_UDP_L4_ipfix)
+if [ "$response" == 200 ] 
+  then 
+  #Update Irule Prime, make sure user clone the new repo
+  curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH -d @Tetration_UDP_L4_ipfix.json https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule/Tetration_UDP_L4_ipfix | python -m json.tool
+  echo "\033[1m UDP Irule exists on BIG-IP already \033[0m \c"
+else
+  sleep 2
+  curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X POST -d @Tetration_UDP_L4_ipfix.json https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule | python -m json.tool
+fi
+tcpresponse=$(curl --write-out %{http_code} --silent --output /dev/null -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule/Tetration_TCP_L4_ipfix)
+if [ "$tcpresponse" == 200 ]
+  then
+  #Update Irule Prime, make sure user has Prime irule locally by cloning
+  echo "\033[1m TCP Irule exists on BIG-IP already \033[0m \c"
+  curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH -d @Tetration_TCP_L4_ipfix.json https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule/Tetration_TCP_L4_ipfix | python -m json.tool
+else
+  sleep 2
+curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X POST -d @Tetration_TCP_L4_ipfix.json https://$BIGIP_MGMT_IP/mgmt/tm/ltm/rule | python -m json.tool
+fi
+
+
+#Check if the IPFIX Pool exists on BIG-IP
+echo " \033[1mChecking if IPX Pool exists  on BIG-IP for Tetration Collector ....\033[0m "
+sleep 2
+curl -sku admin:admin -H "Content-Type: application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool  | python -m json.tool >> pool.txt
+awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="name"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' pool.txt  >> allpool.txt # look for ipfix pools name in the only_name file
+#clean up blank spaces for the pool file
+sed 's/"//g' allpool.txt >> betterpool.txt
+# Load file into array.
+let i=0
+# Read file betterpool.txt line by line and put the details in array 
+while IFS=$'\n' read -r line_data; do
+    poolarray[i]="${line_data}"
+    ((++i))
+done < betterpool.txt
+    let i=0
+    temp=${poolarray[i++]}
+
+if [ "$temp" = "IPFIXPool" ]
+      then
+      echo " \033[0;32mIPFIX Pool exists on your BIGIP :---> $temp \033[0m"
+      sleep 2
+      # Add new Sensors or pool members for IPFIX
+      curl -sku admin:admin -H "Content-Type: application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/~Common~$temp/members  | python -m json.tool >> members.txt
+      awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="address"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' members.txt  >> allmembers.txt
+      sed 's/"//g' allmembers.txt >> bettermembers.txt
+      let i=0
+      # Read file betterpool.txt line by line and put the details in array 
+      while IFS=$'\n' read -r line_data; do
+      membersarray[i]="${line_data}"
+      ((++i))
+      done < bettermembers.txt
+                        let i=0
+      					while (( ${#membersarray[@]} > i )); do
+                        printf "\033[0;32m You have following IPFIX  Members :-->  ${membersarray[i++]}\033[0m \n"
+                        done
+      rm *.txt
+      
+      echo " \033[1m Do you want to Replace Sensor or Pool Member .. ? Say y or n \033[0m  \c "
+      read  type_of_reply
+      let i=0
+       while [ "$type_of_reply" = "y" ]; do
+       			echo " \033[1m Enter Pool Member or Sensor Address to Replace from above IPFIX Pool \033[0m \c"
+       			read exist_address
+       			echo " \033[1m Enter Destination Pool Member or Sensor Address to Replace With \033[0m \c"
+                read dest_address
+       					while (( ${#membersarray[@]} > i )); do
+       					comp=${membersarray[i]}
+       					if  [ "$comp" == "$exist_address" ]
+       			 		then
+       			 		curl -sku admin:admin -H "Content-Type: application/json" -X DELETE https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/IPFIXPool/members/${membersarray[i]}:4739  | python -m json.tool
+                 		curl -sku admin:admin -H "Content-Type: application/json" -X POST https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/~Common~$temp/members -d '{"name":"'${dest_address}':4739", "address":"'$dest_address'"}' | python -m json.tool  
+      			        fi
+      			        let i++
+      			        done
+      			let i=0        
+      			curl -sku admin:admin -H "Content-Type: application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/~Common~$temp/members  | python -m json.tool >> members.txt
+      			awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="address"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' members.txt  >> allmembers.txt
+      			sed 's/"//g' allmembers.txt >> bettermembers.txt
+      			# Read file betterpool.txt line by line and put the details in array 
+      			while IFS=$'\n' read -r line_data; do
+      			membersarray[i]="${line_data}"
+      			((++i))
+      			done < bettermembers.txt
+                        let i=0
+      					while (( ${#membersarray[@]} > i )); do
+                        printf "\033[0;32m You have following IPFIX  Members :-->  ${membersarray[i++]}\033[0m \n"
+                        done
+                rm *.txt
+                 echo " \033[1m Do you want to Replace Sensor or Pool Member .. ? Say y or n \033[0, \c "
+      			read  type_of_reply
+                let i=0
+        done        
+
+
+
+  else
+  	      echo "\033[1m IPFIX Pool is not configured on your BIG-IP \033[0m ..... "
+  	      sleep 1
+  	      echo "\033[1m Enter Pool Member or Sensor Address  \033[0m \c"
+  	      read FirstAddress
+  	      echo "\033[1m Enter Pool Member or Sensor Address \033[0m \c "
+  	      read member_address1
+  	      echo "\033[1m Enter Pool Member or Sensor Address \033[0m \c "
+  	      read member_address2
+          sleep 2
+          ### Create IPXPool required for the tetration collector, this is dedicated Pool
+          echo "\033[1m Creating IPXPool on BIG-IP required for Tetration Collector .......\033[0m \c"
+          sleep 2  
+          curl -sku admin:admin -H "Content-Type: application/json" -X POST  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool -d '{"name": "IPFIXPool", "monitor": "gateway_icmp ", "members": [{"name":"'${FirstAddress}':4739", "address":"'$FirstAddress'"}]}' | python -m json.tool
+          curl -sku admin:admin -H "Content-Type: application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool  | python -m json.tool >> pool.txt
+		  awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="name"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' pool.txt  >> allpool.txt # look for ipfix pools name in the only_name file
+		  #clean up blank spaces for the pool file
+		  sed 's/"//g' allpool.txt >> betterpool.txt
+		  # Load file into array.
+		  let i=0
+		  # Read file betterpool.txt line by line and put the details in array 
+		  while IFS=$'\n' read -r line_data; do
+    	  poolarray[i]="${line_data}"
+          ((++i))
+          done < betterpool.txt
+          let i=0
+          temp=${poolarray[i]}
+          curl -s -sku admin:admin -H "Content-Type: application/json" -X POST https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/~Common~$temp/members -d '{"name":"'${member_address1}':4739", "address":"'$member_address1'"}' > /dev/null  
+          curl -s -sku admin:admin -H "Content-Type: application/json" -X POST https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/~Common~$temp/members -d '{"name":"'${member_address2}':4739", "address":"'$member_address2'"}' > /dev/null  
+          curl -sku admin:admin -H "Content-Type: application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool/~Common~$temp/members  | python -m json.tool >> members.txt
+          awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="address"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' members.txt  >> allmembers.txt
+          sed 's/"//g' allmembers.txt >> bettermembers.txt
+          let i=0
+          # Read file betterpool.txt line by line and put the details in array 
+          while IFS=$'\n' read -r line_data; do
+          membersarray[i]="${line_data}"
+          ((++i))
+          done < bettermembers.txt
+          let i=0
+          while (( ${#membersarray[@]} > i )); do
+          printf "\033[0;32m You have following IPFIX  Members :-->  ${membersarray[i++]}\033[0m \n"
+          done
+          rm *.txt 
+          sleep 0.5
+          echo "curl -sku admin:admin -H "Content-Type: application/json" -X POST  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/pool -d '{"name": "IPFIXPool", "monitor": "gateway_icmp ", "members": [{"name":"'${FirstAddress}':4739", "address":"'$FirstAddress'"}]}' | python -m json.tool"
+
+          ### Creating ... publisher
+          echo "Creating .... IPFIX Publisher ......"
+          sleep 2  
+          curl -sku admin:admin -H "Content-Type: application/json" -X POST  https://$BIGIP_MGMT_IP/mgmt/tm/sys/log-config/destination/ipfix -d '{"name":"IPFIXLog", "poolName":"IPFIXPool","protocolVersion": "ipfix"}' | python -m json.tool
+          sleep 0.5
+          curl -sku admin:admin -H "Content-Type: application/json" -X POST  https://$BIGIP_MGMT_IP/mgmt/tm/sys/log-config/publisher -d '{"name": "ipfix-pub-1", "destinations": [{"name": "IPFIXLog","partition": "Common"}]}' | python -m json.tool
+          sleep 0.5
+fi
+
+#Go through all the virtual servers in BIG-IP
+
+        curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual  | python -m json.tool   >> top.txt
+        sleep 0.5
+
+        #Extract and Print all the virtual servers
+        awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="name"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' top.txt  >> good.txt # look for virtual server name in the only_name file
+        awk -F'[, | ]' '{for(i=1;i<=NF;i++){gsub(/"|:/,"",$i);if($i=="ipProtocol"){gsub(/"|:/,"",$(i+1));print $(i+1)}}}' top.txt  >> goodp.txt # look for virtual server name in the only_name file
+
+        sed 's/"//g' good.txt >> better.txt
+        sed 's/"//g' goodp.txt >> betterp.txt
+        # Load file into array.
+        let i=0
+        # Read file better.txt line by line and put the details in array 
+        while IFS=$'\n' read -r line_data; do
+          viparray[i]="${line_data}"
+          ((++i))
+        done < better.txt
+        let j=0
+        while IFS=$'\n' read -r line_data; do
+            protoarray[j]="${line_data}"
+            ((++j))
+        done < betterp.txt
+
+
+# Explicitly report array content ie the virtual server list
+let i=0
+### Select the Virtual Server to apply the iRule
+while :
+do
+	printf "\033[0;32mYou have following Virtual Server  ${viparray[*]}\033[0m \n"
+echo "Do you wish to apply ipfix irule to all  Virtual Server  say y or n...? "
+read vs_input
+if [ "$vs_input" = "y" ]
+        then 
+        echo " Attaching irule to all Virtual Servers ....."
+        #Extract protocol for Virtual servers
+   
+         
+
+        let j=0
+        while (( ${#protoarray[@]} > j )); do
+             if [ "${protoarray[j]}" == "tcp" ]
+             then
+             curl -k --user admin:admin -H " Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} | python -m json.tool 
+             echo "Attaching tcp irule .................."
+             sleep 1
+             curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} -d '{"rules":["/Common/Tetration_TCP_L4_ipfix"]}'  | python -m json.tool
+             fi   
+            if [ "${protoarray[j]}" == "udp" ]
+            then
+            echo " Attaching udp irule to virtual servers ......."
+            curl -k --user admin:admin -H " Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} | python -m json.tool 
+            curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]}  -d '{"rules":["/Common/Tetration_UDP_L4_ipfix"]}'  | python -m json.tool
+             fi  
+        let "j++"
+       done
+       exit 
+    
+fi
+ 
+let j=0
+while (( ${#protoarray[@]} > j )); do
+                                        
+	                                                   printf "\033[0;32mDo you wish to attach iRule to the Virtual Server say y or n  ${viparray[j]}\033[0m ...? \n"
+	                                                   read answer
+                                                      if [ "$answer" = "y" ]
+                                                      then 
+                                                            if [ "${protoarray[j]}" == "tcp" ]
+                                                            then
+                                                            curl -k --user admin:admin -H " Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} | python -m json.tool 
+                                                            echo "Attaching tcp irule .................."
+                                                            sleep 2
+                                                            curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} -d '{"rules":["/Common/Tetration_TCP_L4_ipfix"]}'  | python -m json.tool
+                                                            fi
+                                                            if [ "${protoarray[j]}" == "udp" ]
+                                                            then
+                                                            echo " Attaching udp irule to virtual servers ......."
+                                                            curl -k --user admin:admin -H " Accept: application/json" -H "Content-Type:application/json" -X GET  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]} | python -m json.tool 
+                                                            curl -k --user admin:admin -H "Accept: application/json" -H "Content-Type:application/json" -X PATCH  https://$BIGIP_MGMT_IP/mgmt/tm/ltm/virtual/${viparray[j]}  -d '{"rules":["/Common/Tetration_UDP_L4_ipfix"]}'  | python -m json.tool
+                                                            fi
+                                                 
+                                                      fi
+                                                      let j++
+
+                                            done
+                                            exit
+                                          
+rm *.txt
+
+done 
+exit                                                                                                             
+
+
+done  # Done for the first While Loop 
